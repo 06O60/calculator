f9be2984406da836427a8a6b57600c7b616c1dcc
Aleksandra Kierska <aleksandrakierska8@gmail.com>
2022-08-25 12:31:35 +0200

    Add calc.js

---
+++ calc.js
@@ -0,0 +1,7 @@
+let result = 0;
+let secondNum = -1;
+let actionClicked = "n";
+
+document.querySelector('.calculator-body').addEventListener('click', function(event) {
+    console.log(event.target.className)
+});
