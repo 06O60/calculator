98e22d8d3d95e2a1553a9cce7eaa57c0bba8bc29
Aleksandra Kierska <aleksandrakierska8@gmail.com>
2022-08-25 12:41:49 +0200

    Make clicked buttons distinguishable

--- calc.js
+++ calc.js
@@ -1,7 +1,44 @@
 let result = 0;
 let secondNum = -1;
-let actionClicked = "n";
+let isActionClicked = "n";
 
 document.querySelector('.calculator-body').addEventListener('click', function(event) {
-    console.log(event.target.className)
+    const clickedClass = event.target.className;
+    if(clickedClass == "action")
+    {
+        actionClicked(event.target.innerText);
+    }
+    else if(clickedClass == "num")
+    {
+        numClicked( parseInt(event.target.innerText) )
+    }
+    else if(clickedClass == "clear")
+    {
+        clearClicked();
+    }
+    else if(clickedClass == "erase")
+    {
+        eraseClicked();
+    }
 });
+
+
+function actionClicked (action)
+{
+    console.log("Action clicked!");
+}
+
+function numClicked (number)
+{
+    console.log("Number clicked!");
+}
+
+function clearClicked ()
+{
+    console.log("Clear clicked!");
+}
+
+function eraseClicked ()
+{
+    console.log("Erase clicked!");
+}
